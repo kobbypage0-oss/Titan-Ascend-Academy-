a02f2347240da313656cdaeffda66bfa286181db
kobbypage0-oss <kobbypage0@gmail.com>
2026-04-28 21:48:30 -0400

    Update Firebase.ts
    Update

--- Lib/Firebase.ts
+++ Lib/Firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "YOUR_KEY",
@@ -9,4 +11,7 @@ const firebaseConfig = {
   appId: "YOUR_APP_ID",
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
