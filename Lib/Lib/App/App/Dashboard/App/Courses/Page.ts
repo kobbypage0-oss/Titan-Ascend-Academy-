894f94f26204884612a647224bab464381da4beb
kobbypage0-oss <kobbypage0@gmail.com>
2026-04-28 21:43:28 -0400

    Create Page.ts

---
+++ Lib/Lib/App/App/Dashboard/App/Courses/Page.ts
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h2>Student Dashboard</h2>
+      <p>Skill Score: 72</p>
+      <p>Current Course: Respiratory Therapy</p>
+    </div>
+  );
+}
