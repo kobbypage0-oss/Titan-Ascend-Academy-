c40abb3e7112357353f0051f489797e01f29261a
kobbypage0-oss <kobbypage0@gmail.com>
2026-04-28 21:41:18 -0400

    Create Page.ts

---
+++ Lib/Lib/App/Page.ts
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Titan Ascend Academy</h1>
+      <p>Train. Ascend. Become Employable.</p>
+    </main>
+  );
+}
